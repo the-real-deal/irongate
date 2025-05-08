d60259c55e74d146e3c7a46a4c172d94941c14eb
Rebecca Scarcelli <rebeccascarcelli5@gmail.com>
2025-05-08 11:33:41 +0000

    tmp

--- db/init/0-schema.sql
+++ db/init/0-schema.sql
@@ -1,60 +1,76 @@
 -- this file creates the database schema
-
 DROP DATABASE IF EXISTS irongate;
 CREATE DATABASE irongate;
 USE irongate;
-
 DROP TABLE IF EXISTS Example;
-CREATE TABLE Example (
-    ID INT NOT NULL,
-    PRIMARY KEY (ID)
-);
+CREATE TABLE Example (ID INT NOT NULL, PRIMARY KEY (ID));
 
 -- Rebe: persona, personale...
 DROP TABLE IF EXISTS Person;
 CREATE TABLE Person (
-    DocNumber PRIMARY KEY (DocNumber),
-    `Name` CHAR[25],
-    Surname CHAR[25],
+    DocNumber INT PRIMARY KEY,
+    `Name` VARCHAR(25) NOT NULL,
+    Surname VARCHAR(25),
     Birthday DATE,
-    BirthPlace CHAR[50],
-)
+    BirthPlace VARCHAR (50),
+    Sex REFERENCES Gender (ID)
+);
+
+DROP TABLE IF EXISTS Gender;
+CREATE TABLE Gender (
+    ID INT PRIMARY KEY,
+    Descr VARCHAR (10)
+);
 
 DROP TABLE IF EXISTS Inmate;
 CREATE TABLE Inmate (
-    ID PRIMARY KEY (ID),
+    ID INT SERIAL PRIMARY KEY,
     IncarcerationDate DATE,
     SentenceDuration YEAR,
-    CriminalRecord CHAR[100],
-)
-
-DROP TABLE IF EXISTS Personell;
-CREATE TABLE Personell (
-    ID PRIMARY KEY (ID),
-    TypeID REFERENCES PersonellType(ID)
-)
-
-DROP TABLE IF EXISTS PersonellType;
-CREATE TABLE PersonellType (
-    ID INT PRIMARY KEY (ID),
-    Descr CHAR[5]
-)
+    CriminalRecord VARCHAR (100),
+);
 
 DROP TABLE IF EXISTS Guest;
 CREATE TABLE Guest (
     DocNumber REFERENCES Person (DocNumber) PRIMARY KEY
-)
+);
+
+DROP TABLE IF EXISTS Visit;
+CREATE TABLE Visit (
+    `DateTime` DATETIME,
+    VisitedInmate REFERENCES Inmate(ID);
+    PRIMARY KEY (`DateTime`, VisitedInmate)
+);
+
+DROP TABLE IF EXISTS Visitors;
+CREATE TABLE Visitors (
+    Visit REFERENCES Visit (`DateTime`, VisitedInmate),
+    Guest REFERENCES Guest (DocNumber),
+    PRIMARY KEY (Visit, Guest)
+);
+
+DROP TABLE IF EXISTS Personell;
+CREATE TABLE Personell (
+    ID INT SERIAL PRIMARY KEY,
+    TypeID REFERENCES PersonellType(ID)
+);
+
+DROP TABLE IF EXISTS PersonellType;
+CREATE TABLE PersonellType (
+    ID INT PRIMARY KEY,
+    Descr VARCHAR (5)
+);
 
 DROP TABLE IF EXISTS Courier;
 CREATE TABLE Courier (
     DocNumber REFERENCES Person (DocNumber) PRIMARY KEY
-)
+);
 
 DROP TABLE IF EXISTS Vehicle;
 CREATE TABLE Vehicle (
-    PlateNumber CHAR[10] PRIMARY KEY (PlateNumber),
+    PlateNumber VARCHAR (10) PRIMARY KEY,
     Driver REFERENCES Courier(DocNumber)
-)
+);
 
 DROP TABLE IF EXISTS Delivery;
 CREATE TABLE Delivery (
@@ -62,19 +78,20 @@ CREATE TABLE Delivery (
     Goods REFERENCES GoodsType(ID),
     Quantity INT,
     PRIMARY KEY (`DateTime`, Goods)
-)
+);
 
 DROP TABLE IF EXISTS GoodsType;
 CREATE TABLE GoodsType (
     ID INT PRIMARY KEY (ID),
-    Descr CHAR[50]
-)
+    Descr VARCHAR (50)
+);
+
 -- Manu: Settore, Zone ...
 
 DROP TABLE IF EXISTS Sector;
 CREATE TABLE Sector (
     ID INT PRIMARY KEY,
-    `Name` CHAR[20],
+    `Name` CHAR (20),
     SecurityLevel INT,
     Capacity INT NOT NULL,
 );
@@ -88,21 +105,17 @@ CREATE TABLE Cell (
 DROP TABLE IF EXISTS Activity;
 CREATE TABLE Activity(
     ID INT PRIMARY KEY (ID),
-    `Name` CHAR[25] NOT NULL,
-    Descr CHAR[200],
+    `Name` CHAR (25) NOT NULL,
+    Descr CHAR (200),
     ActDuration DATETIME,
 );
 
 DROP TABLE IF EXISTS Routine;
-CREATE TABLE Routine (
-    `Datetime` DATETIME,
-    
-);
-
+CREATE TABLE Routine (`Datetime` DATETIME,);
 DROP TABLE IF EXISTS Zone;
 CREATE TABLE Zone (
     ZoneNumber REFERENCES Sector (ID) PRIMARY KEY,
-    `Name` CHAR[25] NOT NULL,
+    `Name` CHAR (25) NOT NULL,
     Capacity INT,
 );
 
